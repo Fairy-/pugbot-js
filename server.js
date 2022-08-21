14a763f442cd4af3af7b368fbe0e6e9e979f9686
Fairy <git@ragetroops.eu>
2022-08-21 14:12:55 +0200

    Promise shenanigans

--- server.js
+++ server.js
@@ -97,9 +97,9 @@ async function queueCleanup() {
 			if((Date.now() - timeout) > player.createtimestamp) {
 				console.log(`Removing ${player.name} from the queue due to inactivity.`);
 				await client.users.fetch(player.id).then((user) => {
-						await user.send("Removing you from the PUG queue due to inactivity.").catch(console.log(`Can't send DM to user ${player.name}. Skipping.`));
-						newresult.delete(player.id);
-				});
+						user.send("Removing you from the PUG queue due to inactivity.")		
+				}).catch(console.log(`Can't send DM to user ${player.name}. Skipping.`));;
+				newresult.delete(player.id);
 			}
 		}
 		client.user.setPresence({ 
